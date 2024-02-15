29f39fcd29b92c46607e117d24f7eeebe5c25f00
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2024-02-15 16:43:35 -0500

    slack stub

--- libbootstrap.sh
+++ libbootstrap.sh
@@ -334,6 +334,27 @@ function InstallDoctl {
     fi
 }
 
+function InstallSlack {
+    echo "TASK: InstallSlack"
+
+    currentVersion="4.36.140"
+    filename="slack-desktop-$currentVersion-amd64.deb"
+
+    slackCheck=$(sudo apt list slack-desktop 2>/dev/null | grep installed)
+    if [ "$slackCheck" == "" ]; then
+        echo "test downloading slack"
+        curl -sS https://downloads.slack-edge.com/releases/linux/$currentVersion/prod/x64/$filename
+        echo "test slack done"
+        return 0
+        #echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list &>/dev/null
+
+        #sudo apt update &>/dev/null
+        #aptUpdated=true
+
+        #CheckForPackageAndInstallIfMissing spotify-client
+    fi
+}
+
 function InstallAdditionalSoftware {
     echo "TASK: InstallAdditionalSoftware"
 
