31f5ea35de1ad29a6343c7663a5a6a5849aa31ca
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-02-18 19:08:26 -0500

    Cleanup (#2)
    * remove gitignore
    
    * cleanup readme
    
    * rename script
    
    * readme
    
    * misc
    
    ---------
    
    Co-authored-by: Cristian Widenhouse <cristian.widenhouse@moovila.com>

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -9,9 +9,9 @@ server=false
 if [ $# -gt 1 ]; then
     printf "\nUsage:\n\n"
     printf "# Runs all tasks\n"
-    printf "./bootstrap.sh\n\n"
+    printf "./bootstrap-debian.sh\n\n"
     printf "# Runs specified task only\n"
-    printf "./bootstrap.sh TASK_NAME\n\n"
+    printf "./bootstrap-debian.sh TASK_NAME\n\n"
     exit 1
 fi
 
