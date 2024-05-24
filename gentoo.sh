f3d4308debcdc8958f430c40d38e49112bd762f2
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-05-24 17:17:08 -0400

    Gtk update, reformat (#15)
    * add comments to help remember
    
    * reindent i guess, update gtk retrieval to work again
    
    * actually just reformat everything
    
    ---------
    
    Co-authored-by: Cristian Widenhouse <cristian.widenhouse@moovila.com>

--- gentoo.sh
+++ gentoo.sh
@@ -5,195 +5,195 @@ source ./libgentoo.sh
 
 # Validate script arguments
 if [ $# -gt 1 ]; then
-    printf "\nUsage:\n\n"
-    printf "# Runs all tasks\n"
-    printf "./gentoo.sh\n\n"
-    printf "# Runs specified task only\n"
-    printf "./gentoo.sh TASK_NAME\n\n"
-    exit 1
+	printf "\nUsage:\n\n"
+	printf "# Runs all tasks\n"
+	printf "./gentoo.sh\n\n"
+	printf "# Runs specified task only\n"
+	printf "./gentoo.sh TASK_NAME\n\n"
+	exit 1
 fi
 
 printf "\n"
 
 if [ $# -eq 1 ]; then
-    case $1 in
-        "CreateDirectories")
-            CreateDirectories
-            ;;
-        "InstallDesktopEnvironment")
-            InstallDesktopEnvironment
-            ;;
-        "InstallMATE")
-            InstallMATE
-            ;;
-        "InstallQtile")
-            InstallQtile
-            ;;
-        "InstallZsh")
-            InstallZsh
-            ;;
-        "InstallCoreUtilities")
-            InstallCoreUtilities
-            ;;
-        "ConfigureTmux")
-            ConfigureTmux
-            ;;
-        "ConfigureZsh")
-            ConfigureZsh
-            ;;
-        "InstallFirefoxBin")
-            InstallFirefoxBin
-            ;;
-        "InstallNvm")
-            InstallNvm
-            ;;
-        "InstallPipewire")
-            InstallPipewire
-            ;;
-        "InstallFonts")
-            InstallFonts
-            ;;
-        "InstallUlauncher")
-            InstallUlauncher
-            ;;
-        "DownloadTheming")
-            DownloadTheming
-            ;;
-        "InstallDotNetCore")
-            InstallDotNetCore
-            ;;
-        "InstallFlatpak")
-            InstallFlatpak
-            ;;
-        "EnsureAppImage")
-            EnsureAppImage
-            ;;
-        "InstallEmacs")
-            InstallEmacs
-            ;;
-        "InstallObsStudio")
-            InstallObsStudio
-            ;;
-        "InstallLibreOffice")
-            InstallLibreOffice
-            ;;
-        "InstallVirtManager")
-            InstallVirtManager
-            ;;
-        "InstallDBeaver")
-            InstallDBeaver
-            ;;
-        "InstallAws")
-            InstallAws
-            ;;
-        "InstallAdditionalSoftware")
-            InstallAdditionalSoftware
-            ;;
-        "InstallWebBrowsers")
-            InstallWebBrowsers
-            ;;
-        *)
-            printf "ERROR: Unknown task\n\n"
-            exit 1
-            ;;
-    esac
+	case $1 in
+	"CreateDirectories")
+		CreateDirectories
+		;;
+	"InstallDesktopEnvironment")
+		InstallDesktopEnvironment
+		;;
+	"InstallMATE")
+		InstallMATE
+		;;
+	"InstallQtile")
+		InstallQtile
+		;;
+	"InstallZsh")
+		InstallZsh
+		;;
+	"InstallCoreUtilities")
+		InstallCoreUtilities
+		;;
+	"ConfigureTmux")
+		ConfigureTmux
+		;;
+	"ConfigureZsh")
+		ConfigureZsh
+		;;
+	"InstallFirefoxBin")
+		InstallFirefoxBin
+		;;
+	"InstallNvm")
+		InstallNvm
+		;;
+	"InstallPipewire")
+		InstallPipewire
+		;;
+	"InstallFonts")
+		InstallFonts
+		;;
+	"InstallUlauncher")
+		InstallUlauncher
+		;;
+	"DownloadTheming")
+		DownloadTheming
+		;;
+	"InstallDotNetCore")
+		InstallDotNetCore
+		;;
+	"InstallFlatpak")
+		InstallFlatpak
+		;;
+	"EnsureAppImage")
+		EnsureAppImage
+		;;
+	"InstallEmacs")
+		InstallEmacs
+		;;
+	"InstallObsStudio")
+		InstallObsStudio
+		;;
+	"InstallLibreOffice")
+		InstallLibreOffice
+		;;
+	"InstallVirtManager")
+		InstallVirtManager
+		;;
+	"InstallDBeaver")
+		InstallDBeaver
+		;;
+	"InstallAws")
+		InstallAws
+		;;
+	"InstallAdditionalSoftware")
+		InstallAdditionalSoftware
+		;;
+	"InstallWebBrowsers")
+		InstallWebBrowsers
+		;;
+	*)
+		printf "ERROR: Unknown task\n\n"
+		exit 1
+		;;
+	esac
 
-    printf "\n"
-    exit 0
+	printf "\n"
+	exit 0
 fi
 
 # Full run, exit if a task errors
 
 if ! CreateDirectories; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallDesktopEnvironment; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallZsh; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallCoreUtilities; then
-    exit 1
+	exit 1
 fi
 
 if ! ConfigureTmux; then
-    exit 1
+	exit 1
 fi
 
 if ! ConfigureZsh; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallFirefoxBin; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallNvm; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallPipewire; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallFonts; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallUlauncher; then
-    exit 1
+	exit 1
 fi
 
 if ! DownloadTheming; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallDotNetCore; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallFlatpak; then
-    exit 1
+	exit 1
 fi
 
 if ! EnsureAppImage; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallEmacs; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallObsStudio; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallLibreOffice; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallVirtManager; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallDBeaver; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallAws; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallAdditionalSoftware; then
-    exit 1
+	exit 1
 fi
 
 if ! InstallWebBrowsers; then
-    exit 1
+	exit 1
 fi
 
 printf "\n"
