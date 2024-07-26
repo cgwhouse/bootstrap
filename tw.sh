b17ee65a8bf7394b5daa477013a44567f5aa7c45
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2024-07-26 18:28:57 -0400

    wip

--- tw.sh
+++ tw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-source "$SCRIPT_DIR"/libfedora.sh
+source "$SCRIPT_DIR"/libtw.sh
 
 if [ $# -gt 0 ]; then
 	printf "\nUsage:\n\n"
